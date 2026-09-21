8fdde107b9a16c4274006bcbdefe0150c7eb82b2
Aitzaz ul hassan <231863952+aitzazhassan54@users.noreply.github.com>
2026-09-21 10:58:38 +0500

    Part 2: Course & Roster Data

--- course_roster.dart
+++ course_roster.dart
@@ -3,6 +3,26 @@ void printWelcome(String appName) {
   print('=== $appName ===');
 }
 
+// void main() {
+//   printWelcome('Course Roster Manager');
+// }
+
+// Part 2
+
 void main() {
   printWelcome('Course Roster Manager');
+
+  const int maxCapacity = 4; // true compile-time constant
+  final DateTime createdAt = DateTime.now(); // known only at runtime, set once
+  String courseTitle = 'CS201: Mobile App Development';
+  int capacity = maxCapacity;
+  double creditHours = 3.0;
+  bool isOpen = true;
+  List<String> enrolledStudents = ['Aitzaz', 'ahmad', 'Mehdi'];
+  Set<String> waitlist = {'saqib', 'waseel'};
+  Map<String, int> attendanceCount = {'Aitzaz': 3, 'ahmad': 4, 'Mehdi': 2};
+
+  print(
+    '$courseTitle | Capacity: $capacity | Enrolled: ${enrolledStudents.length}',
+  );
 }
